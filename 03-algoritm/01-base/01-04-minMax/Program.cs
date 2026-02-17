3336a8a2378c0404158f6b85650e7cb438629a81
odenec <viktorduboshit@ya.ru>
2026-02-17 13:39:38 +0300

    add max,min base algoritm

---
+++ 03-algoritm/01-base/01-04-minMax/Program.cs
@@ -0,0 +1,25 @@
+﻿int maxnumber = MaxMinAlgoritm.Maximum([1, 3, 4]);
+Console.WriteLine(maxnumber);
+
+class MaxMinAlgoritm
+{
+    public static int Maximum(int[] numbers)
+    {
+        int maxNumber = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            maxNumber = numbers[i] > maxNumber ? numbers[i] : maxNumber;
+        }
+        return maxNumber;
+    }
+
+    public int Minimum(int[] numbers)
+    {
+        int minNumber = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            minNumber = numbers[i] < minNumber ? numbers[i] : minNumber;
+        }
+        return minNumber;
+    }
+}
